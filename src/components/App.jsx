a16c6e2fcdfbf9369dba2b3fc00c44b5e20896e3
Job Mwitah <jobmwitah@gmail.com>
2023-07-17 18:42:03 +0300

    use React18 syntax on app.js and index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => {
-  return <div>App</div>;
+const MovieInformation = () => <h1>Movie Information</h1>;
+const Actors = () => <h1>Actors</h1>;
+const Movies = () => <h1>Movies</h1>;
+const Profile = () => <h1>Profile</h1>;
+
+const App = function () {
+  return (
+    <div>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route path="/movies/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
