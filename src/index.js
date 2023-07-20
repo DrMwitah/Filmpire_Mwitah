f079cbdbcc33c1bc5c3621126698db4536cd3488
Job Mwitah <jobmwitah@gmail.com>
2023-07-20 15:04:06 +0300

    add sidebar and logo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import App from './components/App';
 
+const theme = createTheme({});
+
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
